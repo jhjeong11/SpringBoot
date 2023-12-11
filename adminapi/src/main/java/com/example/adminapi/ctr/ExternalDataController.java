81c453caa54f8d677b674c7cff1d3cf6ebfd125b
jh.jeong <jh.jeong@okestro.com>
2023-12-11 10:30:08 +0900

    2023.12.11

--- adminapi/src/main/java/com/example/adminapi/ctr/ExternalDataController.java
+++ adminapi/src/main/java/com/example/adminapi/ctr/ExternalDataController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:8083")
+@CrossOrigin(origins = "http://localhost:8084")
 public class ExternalDataController {
 
     @GetMapping("/external-data")
