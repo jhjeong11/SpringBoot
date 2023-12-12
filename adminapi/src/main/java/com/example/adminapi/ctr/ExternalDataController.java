515d44f3e9d0e408c1582e0dfeea24eb53960e51
jh.jeong <jh.jeong@okestro.com>
2023-12-12 10:18:53 +0900

    2023.12.12

--- adminapi/src/main/java/com/example/adminapi/ctr/ExternalDataController.java
+++ adminapi/src/main/java/com/example/adminapi/ctr/ExternalDataController.java
@@ -2,10 +2,7 @@ package com.example.adminapi.ctr;
 
 import com.example.adminapi.domain.ExternalData;
 import com.example.adminapi.domain.ExternalDataProvider;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +14,10 @@ public class ExternalDataController {
     @GetMapping("/external-data")
     public List<ExternalData> getExternalData() {
         return ExternalDataProvider.getExternalData();
-    }
+        }
 
-}
+        @GetMapping("/external-data/{id}")
+        public ExternalData getExternalDataById(@PathVariable String id) {
+        return ExternalDataProvider.getExternalDataById(id);
+        }
+    }
--- adminapi/src/main/java/com/example/adminapi/domain/ExternalDataProvider.java
+++ adminapi/src/main/java/com/example/adminapi/domain/ExternalDataProvider.java
@@ -5,23 +5,31 @@ import java.util.List;
 
 public class ExternalDataProvider {
 
-    public static List<ExternalData> getExternalData() {
-        List<ExternalData> externalDataList = new ArrayList<>();
+    private static List<ExternalData> externalDataList = new ArrayList<>();
 
+    static {
         externalDataList.add(new ExternalData("1", "첫번째", "정주희", new ArrayList<>()));
         externalDataList.add(new ExternalData("2", "두번째", "정주희", new ArrayList<>()));
         externalDataList.add(new ExternalData("3", "세번째", "정주희", new ArrayList<>()));
         externalDataList.add(new ExternalData("4", "네번째", "정주희", new ArrayList<>()));
-        externalDataList.add(new ExternalData("5", "다섯번째", "정주희", new ArrayList<>()));
-        externalDataList.add(new ExternalData("6", "여섯번째", "정주희", new ArrayList<>()));
-        externalDataList.add(new ExternalData("7", "일곱번째", "정주희", new ArrayList<>()));
-        externalDataList.add(new ExternalData("8", "여덟번째", "정주희", new ArrayList<>()));
-        externalDataList.add(new ExternalData("9", "아홉번째", "정주희", new ArrayList<>()));
+        externalDataList.add(new ExternalData("5", "다섯 번째", "정주희", new ArrayList<>()));
+        externalDataList.add(new ExternalData("6", "여섯 번째", "정주희", new ArrayList<>()));
+        externalDataList.add(new ExternalData("7", "일곱 번째", "정주희", new ArrayList<>()));
+        externalDataList.add(new ExternalData("8", "여덟 번째", "정주희", new ArrayList<>()));
+        externalDataList.add(new ExternalData("9", "아홉 번째", "정주희", new ArrayList<>()));
         externalDataList.add(new ExternalData("10", "열번째", "정주희", new ArrayList<>()));
-        externalDataList.add(new ExternalData("11", "열한번째", "정주희", new ArrayList<>()));
-        externalDataList.add(new ExternalData("12", "열두번째", "정주희", new ArrayList<>()));
+        externalDataList.add(new ExternalData("11", "열 한번째", "정주희", new ArrayList<>()));
+        externalDataList.add(new ExternalData("12", "열 두번째", "정주희", new ArrayList<>()));
+    }
 
-
+    public static List<ExternalData> getExternalData() {
         return externalDataList;
+    }
+
+    public static ExternalData getExternalDataById(String id) {
+        return externalDataList.stream()
+                .filter(data -> data.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 }
